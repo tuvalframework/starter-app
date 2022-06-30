956354a0f3546f844fc7c6b90e53e34654b67113
selimtan <stan@bpmgenesis.com>
2022-06-30 17:59:58 +0300

    .

---
+++ dist_types/types/Application.d.ts
@@ -0,0 +1,5 @@
+import { TApplication } from '@tuval/forms';
+export declare class IconLibrary extends TApplication {
+    private m_tbiLabel;
+    InitComponents(): void;
+}
---
+++ dist_types/types/MainForm.d.ts
@@ -0,0 +1,6 @@
+import { TForm } from '@tuval/forms';
+export declare class MainForm extends TForm {
+    m_Toolbar: any;
+    m_tbiLabel: any;
+    InitComponents(): void;
+}
